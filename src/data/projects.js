67e1d7b6e57276ceb16c6ab66d74278897927b3a
James Gardner <james.gardner@usanainc.com>
2022-10-13 12:14:30 -0600

    new react app

---
+++ src/data/projects.js
@@ -0,0 +1,34 @@
+// TODO Add a couple lines about each project
+const data = [
+  {
+    title: 'Developer Deployment App',
+    subtitle: '2021 Deploy Legacy and K8s Deployments',
+    image: '/images/projects/k8sdeployapp.png',
+    date: '2021-01-01',
+    desc:
+      'Built for all development teams to deploy 90+ applications to servers and kubernetes'
+      + 'Interfaces with Bamboo api and Consul api'
+      + 'Written with Angular/Javascript and a node.js backend',
+  },
+  {
+    title: 'Devops Deployment App',
+    subtitle: 'DevOps deployer app for pipeline environments',
+    image: '/images/projects/hadesapp.png',
+    date: '2021-01-01',
+    desc:
+      'Built for DevOps to cut release branches using Bitbucket API and Monitor builds wiht Bamboo Api.'
+      + 'Also used for Deploying 90+ apps to pipeline environments.'
+      + 'Written with Angular/Javascript',
+  },
+  {
+    title: 'Environment Checkout App',
+    subtitle: 'A global location for the company to see who has what environment checkout',
+    image: '/images/projects/envcheckoutapp.png',
+    date: '2021-01-01',
+    desc:
+      'An application to allow QA and Developers to checkout one of 30+ environments.'
+      + 'Written in Angular/Javascript',
+  },
+];
+
+export default data;
---
+++ src/data/resume/degrees.js
@@ -0,0 +1,22 @@
+const degrees = [
+  {
+    school: 'Western Governors University',
+    degree: 'B.S. Information Technology',
+    link: 'https://www.wgu.edu',
+    year: 2013,
+  },
+  {
+    school: 'University of Colorado, Denver',
+    degree: 'B.S. Music Industry Technology',
+    link: 'https://https://www.ucdenver.edu/',
+    year: 2010,
+  },
+  {
+    school: 'Western Wyoming Community College',
+    degree: 'Associate of Arts in Music',
+    link: 'https://www.westernwyoming.edu/',
+    year: 2007,
+  },
+];
+
+export default degrees;
